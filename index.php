339f5d1602448544d567c606668b94c5c77473d5
maria0406 <maria041006cruz@gmail.com>
2023-02-24 15:01:43 -0600

    PHP Server test

--- index.php
+++ index.php
@@ -1,25 +1,10 @@
 <?php
-phpinfo();
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-$name = $_POST['name'];
-$visitor_email = $_POST['email'];
-$message = $_POST['message'];
-
-$email_from = 'dynamicgraphics7@gmail.com';
-$email_subject = "New Form Submission";
-$email_body = "User Name: $name.\n".
-              "User Email: $visitor_email.\n".  
-              "User Message: $message.\n";
-$to = "dynamicgraphics7@gmail.com";
-$headers = "From: $email_from\r\n";
-$headers .= "Reply-To: $visitor_email\r\n";
-
-mail($to, $email_subject, $email_body, $headers);
-echo "Email sent";
-header("Location: index.html");
+if (mail('your-email@example.com', 'Test Email', 'This is a test email.')) {
+  echo 'Mail sent successfully!';
+} else {
+  echo 'Mail failed to send.';
+}
 ?>
+
 
 
